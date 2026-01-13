74f10af668d6d1a096e9abb202df62ba4988a461
wupe1001 <sangwu1104@naver.com>
2026-01-13 16:36:34 +0900

    refactor: BackPageGNB 수정

--- src/components/gnb/BackPageGNB.tsx
+++ src/components/gnb/BackPageGNB.tsx
@@ -7,6 +7,7 @@ export interface BackPageGNBProps {
   className?: string;
   title?: string;
   text?: string;
+  titleColor?: string;
   onBack?: () => void;
   onSkip?: () => void;
 }
@@ -15,30 +16,35 @@ const BackPageGNB: React.FC<BackPageGNBProps> = ({
   className,
   title = '목표 설정하기',
   text = '건너뛰기',
+  titleColor = 'text-neutral-70',
   onBack,
   onSkip,
 }) => {
   return (
-    <header className={cn('flex items-center justify-between w-[360px] h-[50px] px-[20px] bg-neutral-10', className)}>
+    <header
+      className={cn('relative flex items-center justify-between w-[360px] h-[50px] px-[20px] bg-neutral-10', className)}
+    >
       <div className="flex items-center justify-start">
         <button type="button" onClick={onBack} className="flex items-center justify-center cursor-pointer w-[24px]">
           <img src={BackPageIcon} alt="뒤로가기" />
         </button>
       </div>
 
-      <Typography
-        style="text-body-1-16-medium"
-        fontFamily="pretendard"
-        className={cn('text-center text-neutral-70 flex-1')}
-      >
-        {title}
-      </Typography>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Typography style="text-body-1-16-medium" fontFamily="pretendard" className={titleColor}>
+          {title}
+        </Typography>
+      </div>
 
-      <div className="flex items-center justify-end ">
+      <div className="flex items-center justify-end z-10">
         <button type="button" onClick={onSkip}>
-          <Typography style="text-body-3-13-regular" fontFamily="pretendard" className="text-neutral-70 underline">
-            {text}
-          </Typography>
+          {text && (
+            <button type="button" onClick={onSkip}>
+              <Typography style="text-body-3-13-regular" fontFamily="pretendard" className="text-neutral-70 underline">
+                {text}
+              </Typography>
+            </button>
+          )}
         </button>
       </div>
     </header>
