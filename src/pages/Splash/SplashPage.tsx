c1af9dfe689d8846b00986c670a3ef8fe8a74fe1
Dohyeon <do.hy824@gmail.com>
2026-02-11 03:30:02 +0900

    feat: 배경 애니메이션 추가

--- src/pages/Splash/SplashPage.tsx
+++ src/pages/Splash/SplashPage.tsx
@@ -22,15 +22,18 @@ const SplashPage = () => {
       <div className={cn('w-full max-w-[360px] min-h-screen bg-white relative flex flex-col overflow-hidden')}>
         {/* 배경 이미지 */}
         <div
-          className={cn('absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[360px] h-[800px]')}
+          className={cn(
+            'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[360px] h-[800px] animate-float-background'
+          )}
         >
           <img src={splashBg} alt="" className={cn('w-full h-full object-cover')} />
+          <div className={cn('absolute inset-0 animate-shimmer')} />
         </div>
 
         {/* 회전된 이미지 (왼쪽) */}
         <div
           className={cn(
-            'absolute left-[116.87px] top-[343.7px] w-[118.26px] h-[65.615px] flex items-center justify-center'
+            'absolute left-[116.87px] top-[343.7px] w-[118.26px] h-[65.615px] flex items-center justify-center z-10'
           )}
         >
           <div className={cn('-rotate-90')}>
