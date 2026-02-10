95e9e9ca57e1cee20f9fe2fc73428263db5a55bf
Dohyeon <do.hy824@gmail.com>
2026-02-11 03:25:28 +0900

    design: 스플래시 이미지 변경

--- src/pages/Splash/SplashPage.tsx
+++ src/pages/Splash/SplashPage.tsx
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Typography } from '@/components';
 import { cn } from '@/utils/cn';
-import ValuediLogo from '@/assets/icons/ValuediLogo.svg?react';
+import splashBg from '@/assets/images/splash/splash_bg.svg';
+import splashLogo from '@/assets/images/splash/splash_logo.svg';
+import splashRotated from '@/assets/images/splash/splash_rotated.svg';
 
 const SplashPage = () => {
   const navigate = useNavigate();
@@ -17,25 +19,44 @@ const SplashPage = () => {
 
   return (
     <div className={cn('min-h-screen w-full bg-white flex justify-center items-center')}>
-      <div className={cn('w-full max-w-[360px] min-h-screen bg-white relative flex flex-col')}>
-        <div className={cn('flex flex-col items-center justify-center min-h-screen w-full relative')}>
-          <div
-            className={cn(
-              'flex flex-col items-center gap-1 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[200px]'
-            )}
-          >
-            <div className={cn('flex items-center justify-center rounded-[7px]')}>
-              <ValuediLogo className={cn('w-[31px] h-auto')} />
-            </div>
-          </div>
-          <Typography
-            style="text-body-2-14-regular"
-            className={cn('absolute bottom-[83px] text-center w-[320px] left-1/2 -translate-x-1/2')}
-            color="neutral-70"
-          >
-            똑똑한 금융서비스, 밸류디
-          </Typography>
+      <div className={cn('w-full max-w-[360px] min-h-screen bg-white relative flex flex-col overflow-hidden')}>
+        {/* 배경 이미지 */}
+        <div
+          className={cn('absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[360px] h-[800px]')}
+        >
+          <img src={splashBg} alt="" className={cn('w-full h-full object-cover')} />
         </div>
+
+        {/* 회전된 이미지 (왼쪽) */}
+        <div
+          className={cn(
+            'absolute left-[116.87px] top-[343.7px] w-[118.26px] h-[65.615px] flex items-center justify-center'
+          )}
+        >
+          <div className={cn('-rotate-90')}>
+            <img src={splashRotated} alt="" className={cn('w-[65.615px] h-[118.26px]')} />
+          </div>
+        </div>
+
+        {/* 중앙 로고 */}
+        <div
+          className={cn(
+            'flex flex-col items-center justify-center min-h-screen w-full relative z-10'
+          )}
+        >
+          <div className={cn('absolute left-[124px] top-[425.31px] w-[100px] h-[23px]')}>
+            <img src={splashLogo} alt="Valuedi" className={cn('w-full h-full')} />
+          </div>
+        </div>
+
+        {/* 하단 텍스트 */}
+        <Typography
+          style="text-body-2-14-regular"
+          className={cn('absolute bottom-[83px] text-center w-[320px] left-1/2 -translate-x-1/2 z-10')}
+          color="neutral-70"
+        >
+          똑똑한 금융서비스, 밸류디
+        </Typography>
       </div>
     </div>
   );
